6849bade75c10e29ff2f805b92ef35926ebf85f9
adilsxn <24adilsonnunes@gmail.com>
2024-03-05 12:42:56 +0000

    fix double prompt for "cat | cat | ls" followed by CTRL-C

--- src/signals.c
+++ src/signals.c
@@ -12,49 +12,42 @@
 
 #include "../inc/minishell.h"
 
-static void	ignore_the_sigquit(void)
-{
-	struct sigaction	event;
+static void ignore_the_sigquit(void) {
+  struct sigaction event;
 
-	ft_bzero(&event, sizeof(struct sigaction));
-	event.sa_handler = SIG_IGN;
-	sigaction(SIGQUIT, &event, NULL);
+  ft_bzero(&event, sizeof(struct sigaction));
+  event.sa_handler = SIG_IGN;
+  sigaction(SIGQUIT, &event, NULL);
 }
 
-void	sig_hdoc_child(int sig)
-{
-	if (sig == SIGINT)
-	{
-		ft_putchar_fd('\n', 1);
-		exit(SIGINT);
-	}
+void sig_hdoc_child(int sig) {
+  if (sig == SIGINT) {
+    ft_putchar_fd('\n', 1);
+    exit(SIGINT);
+  }
 }
 
-void	sig_hdoc_parent(int sig)
-{
-	(void)sig;
-	rl_redisplay();
+void sig_hdoc_parent(int sig) {
+  (void)sig;
+  rl_redisplay();
 }
 
-void	sig_new_prompt(int sig)
-{
-	if (sig == SIGINT)
-	{
-		ft_putchar_fd('\n', 1);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
-	}
+void sig_new_prompt(int sig) {
+  if (sig == SIGINT) {
+    ft_putchar_fd('\n', 1);
+    // rl_replace_line("", 0);
+    // rl_on_new_line();
+    rl_redisplay();
+  }
 }
 
-void	signal_handler(void (*handler)(int), int signal)
-{
-	struct sigaction	event;
+void signal_handler(void (*handler)(int), int signal) {
+  struct sigaction event;
 
-	ignore_the_sigquit();
-	ft_bzero(&event, sizeof(struct sigaction));
-	event.sa_handler = handler;
-	event.sa_flags = SA_SIGINFO | SA_RESTART;
-	sigemptyset(&event.sa_mask);
-	sigaction(signal, &event, NULL);
+  ignore_the_sigquit();
+  ft_bzero(&event, sizeof(struct sigaction));
+  event.sa_handler = handler;
+  event.sa_flags = SA_SIGINFO | SA_RESTART;
+  sigemptyset(&event.sa_mask);
+  sigaction(signal, &event, NULL);
 }
