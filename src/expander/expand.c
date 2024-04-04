8d9eb48fe526066b663ccb8432980c8ec6c537a2
mati <matde-je@c2r10s8.42lisboa.com>
2024-04-04 21:18:54 +0100

    ok

--- src/expander/expand.c
+++ src/expander/expand.c
@@ -90,7 +90,7 @@ t_lexer	*expander2(t_env *env, t_lexer *lexi)
 	{
 		if (lex->str)
 		{
-			if (lex->i == 0 || (lex->i > 0 && (!lex->prev->token
+			if ((lex->i == 0 && lex->token != 5) || (lex->i > 0 && (!lex->prev->token
 						|| lex->prev->token != 5)))
 			{
 				lex->str = expander(env, lex->str);
